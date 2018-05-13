643f2146bc440958e9cf5a7a721f7b78e9097ec8
jordi <joponet@gmail.com>
2018-05-13 12:56:38 +0200

    Selecció network interface

--- pingpong_client/src/net/pingpong/client/ClientStart.java
+++ pingpong_client/src/net/pingpong/client/ClientStart.java
@@ -1,14 +1,18 @@
 package net.pingpong.client;
 
 import net.pingpong.lib.GameVars;
+import net.pingpong.lib.Interfaces;
 
 public class ClientStart {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GameVars gameVars = new GameVars();
-		gameVars.setNetworkInterface(args[0]);
-		gameVars.setServerAddress(args[1]);
+		Interfaces.listInterfaces();
+		System.out.printf("Selected: %s\n",Interfaces.getInterface().getName());
+//		gameVars.setNetworkInterface(args[0]);
+//		gameVars.setServerAddress(args[1]);
+		gameVars.setNetworkInterface();
 		Game game = new Game(gameVars);
 		game.Run();
 	}
